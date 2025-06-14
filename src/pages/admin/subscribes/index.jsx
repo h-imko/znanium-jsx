1d40b8cf9ce7b1009c4fd66efd7fad8993f9d084
Никита Вариводин <58493296+h-imko@users.noreply.github.com>
2025-06-15 00:28:46 +0300

    /

--- src/pages/admin/subscribes/index.jsx
+++ src/pages/admin/subscribes/index.jsx
@@ -41,8 +41,7 @@ export default function index() {
               sortable: true,
               title: "Док-тов"
             },
-            {
-            },
+            {},
           ],
           rows: [
             [
